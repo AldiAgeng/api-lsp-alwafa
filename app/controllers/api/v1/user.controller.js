050d6a15bb0133aeb72bdc12cb4dbc60f20ce02d
AldiAgeng <aldiageng48@gmail.com>
2022-12-21 21:57:35 +0700

    login accession

--- app/controllers/api/v1/user.controller.js
+++ app/controllers/api/v1/user.controller.js
@@ -1,5 +1,6 @@
 const {Administrator} = require("../../../models");
 const {Assessor} = require("../../../models");
+const {Accession} = require("../../../models");
 const utils = require("../../../utils");
 
 module.exports = {
@@ -17,7 +18,14 @@ module.exports = {
         }
       })
 
-      if(!admin && !assessor){
+      const accession = await Accession.findOne({
+        where:{
+          username: req.body.username
+        }
+      })
+      console.log(admin, assessor, accession)
+
+      if(!admin && !assessor && !accession){
         return res.status(401).json({
           status: "fail",
           statusCode: 401,
@@ -33,7 +41,7 @@ module.exports = {
           status: "fail",
           statusCode: 401,
           name: "Unauthorized",
-          message: "Username or password is wrong coy",
+          message: "Username or password is wrong",
         });
       }
       const token = utils.createToken({
@@ -58,13 +66,37 @@ module.exports = {
             status: "fail",
             statusCode: 401,
             name: "Unauthorized",
-            message: "Username or password is wrong coy",
+            message: "Username or password is wrong",
           });
         }
         const token = utils.createToken({
           id: assessor.id,
           username: assessor.username,
           code_assessor: assessor.code_assessor,
+        });
+
+        res.status(200).json({
+          status: "success",
+          statusCode: 200,
+          message: "User successfully logged in",
+          data: {
+            token,
+          }
+        })
+      }else if(accession){
+        const isPasswordValid = await utils.checkPassword(accession.password, req.body.password)
+        if(!isPasswordValid){
+          return res.status(401).json({
+            status: "fail",
+            statusCode: 401,
+            name: "Unauthorized",
+            message: "Username or password is wrong",
+          });
+        }
+        const token = utils.createToken({
+          id: accession.id,
+          username: accession.username,
+          code_accession: accession.code_accession,
         });
 
         res.status(200).json({

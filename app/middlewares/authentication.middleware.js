cae559d58fdf353dafa8941b80d20a7535229ee8
AldiAgeng <aldiageng48@gmail.com>
2023-01-15 10:31:56 +0700

    add authAdmin middleware

--- app/middlewares/authentication.middleware.js
+++ app/middlewares/authentication.middleware.js
@@ -1,24 +1,58 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const {Administrator} = require("../models");
-module.exports = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split("Bearer ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    
+module.exports = {
+  async authAdmin (req, res, next) {
+    try {
+      const token = req.headers.authorization.split("Bearer ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  
+      req.user = await Administrator.findOne({
+        where: {
+          id: decoded.id,
+          username: decoded.username,
+        },
+      });
 
-    req.user = await Administrator.findOne({
-      where: {
-        id: decoded.id,
-      },
-    });
+      if (!req.user) {
+        return res.status(403).json({
+          status: "fail",
+          statusCode: 403,
+          name: "Forbidden",
+          message: "You are not allowed to access this resource",
+        })
+      }else {
+        next();
+      }
+    } catch (error) {
+      return res.status(401).json({
+        status: "fail",
+        statusCode: 401,
+        name: "Unauthorized",
+        message: "You are not login/access_token is wrong",
+      });
+    }
+  },
+  // async authAssessor (req, res, next) {
+  //   try {
+  //     const token = req.headers.authorization.split("Bearer ")[1];
+  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      status: "fail",
-      statusCode: 401,
-      name: "Unauthorized",
-      message: "You are not login/access_token is wrong",
-    });
-  }
-}
+  //     req.user = await Assessor.findOne({
+  //       where: {
+  //         id: decoded.id,
+  //         username: decoded.username,
+  //       },
+  //     });
+
+  //     next();
+  //   } catch (error) {
+  //     return res.status(401).json({
+  //       status: "fail",
+  //       statusCode: 401,
+  //       name: "Unauthorized",
+  //       message: "You are not login/access_token is wrong",
+  //     });
+  //   }
+  // },
+} 
--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,5 +1,7 @@
 const bodyMiddleware = require("./body.middleware");
+const authMiddleware = require("./authentication.middleware");
 
 module.exports = {
   bodyMiddleware,
+  authMiddleware,
 };

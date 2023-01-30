51b76fe0b48bf465cbb907d80762e0065d696203
AldiAgeng <aldiageng48@gmail.com>
2023-01-30 12:23:45 +0700

    route schedule

--- config/routes.js
+++ config/routes.js
@@ -19,36 +19,45 @@ appRouter.get("/", controllers.main.index);
 apiRouter.post("/api/v1/user/login", controllers.api.v1.user.login);
 
 // Admin
-apiRouter.get("/api/v1/administrator", middlewares.authMiddleware.authAdmin,controllers.api.v1.administrator.getAllAdministrator);
-apiRouter.get("/api/v1/administrator/:id", middlewares.authMiddleware.authAdmin,controllers.api.v1.administrator.getAdministratorById);
-apiRouter.post("/api/v1/administrator", middlewares.authMiddleware.authAdmin,
-  validations.administratorValidation.createValidation, 
-  middlewares.bodyMiddleware, 
-  controllers.api.v1.administrator.createAdministrator
-  );
+apiRouter.get("/api/v1/administrator", middlewares.authMiddleware.authAdmin, controllers.api.v1.administrator.getAllAdministrator);
+apiRouter.get("/api/v1/administrator/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.administrator.getAdministratorById);
+apiRouter.post("/api/v1/administrator", middlewares.authMiddleware.authAdmin, validations.administratorValidation.createValidation, middlewares.bodyMiddleware, controllers.api.v1.administrator.createAdministrator);
 apiRouter.patch("/api/v1/administrator/:id", middlewares.authMiddleware.authAdmin, /* validations.administratorValidation.updateValidation, middlewares.bodyMiddleware, */ controllers.api.v1.administrator.updateAdministrator);
 apiRouter.delete("/api/v1/administrator/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.administrator.deleteAdministrator);
 
 // Admin Manage User
 // Assessor
 apiRouter.get("/api/v1/admin/assessor", middlewares.authMiddleware.authAdmin, controllers.api.v1.assessor.getAllAssessors);
-apiRouter.get("/api/v1/admin/assessor/:id", middlewares.authMiddleware.authAdmin,controllers.api.v1.assessor.getAssessorById);
-apiRouter.post("/api/v1/admin/assessor", middlewares.authMiddleware.authAdmin, validations.assessorValidation.createValidation,middlewares.bodyMiddleware, controllers.api.v1.assessor.createAssessor);
+apiRouter.get("/api/v1/admin/assessor/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.assessor.getAssessorById);
+apiRouter.post("/api/v1/admin/assessor", middlewares.authMiddleware.authAdmin, validations.assessorValidation.createValidation, middlewares.bodyMiddleware, controllers.api.v1.assessor.createAssessor);
 apiRouter.patch("/api/v1/admin/assessor/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.assessor.updateAssessor);
 apiRouter.delete("/api/v1/admin/assessor/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.assessor.deleteAssessor);
 // Accession
 apiRouter.get("/api/v1/admin/accession", middlewares.authMiddleware.authAdmin, controllers.api.v1.accession.getAllAccessions);
 apiRouter.get("/api/v1/admin/accession/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.accession.getAccessionById);
-apiRouter.post("/api/v1/admin/accession", middlewares.authMiddleware.authAdmin, validations.accessionValidation.createValidation,middlewares.bodyMiddleware, controllers.api.v1.accession.createAccession);
+apiRouter.post("/api/v1/admin/accession", middlewares.authMiddleware.authAdmin, validations.accessionValidation.createValidation, middlewares.bodyMiddleware, controllers.api.v1.accession.createAccession);
 apiRouter.patch("/api/v1/admin/accession/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.accession.updateAccession);
 apiRouter.delete("/api/v1/admin/accession/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.accession.deleteAccession);
 
 // Competency Test Place
 apiRouter.get("/api/v1/admin/competency-test-place", middlewares.authMiddleware.authAdmin, controllers.api.v1.competencyTestPlace.getAllCompetencyTestPlaces);
-apiRouter.get("/api/v1/admin/competency-test-place/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.competencyTestPlace.getCompetencyTestPlaceById); 
-apiRouter.post("/api/v1/admin/competency-test-place", middlewares.authMiddleware.authAdmin, validations.competencyTestPlaceValidation.createCompetencyTestPlace, middlewares.bodyMiddleware, controllers.api.v1.competencyTestPlace.createCompetencyTestPlace);
+apiRouter.get("/api/v1/admin/competency-test-place/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.competencyTestPlace.getCompetencyTestPlaceById);
+apiRouter.post(
+  "/api/v1/admin/competency-test-place",
+  middlewares.authMiddleware.authAdmin,
+  validations.competencyTestPlaceValidation.createCompetencyTestPlace,
+  middlewares.bodyMiddleware,
+  controllers.api.v1.competencyTestPlace.createCompetencyTestPlace
+);
 apiRouter.patch("/api/v1/admin/competency-test-place/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.competencyTestPlace.updateCompetencyTestPlace);
 apiRouter.delete("/api/v1/admin/competency-test-place/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.competencyTestPlace.deleteCompetencyTestPlace);
+
+// Schedule
+apiRouter.get("/api/v1/admin/schedule", middlewares.authMiddleware.authAdmin, controllers.api.v1.schedule.getAllSchedules);
+apiRouter.get("/api/v1/admin/schedule/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.schedule.getScheduleById);
+apiRouter.post("/api/v1/admin/schedule", middlewares.authMiddleware.authAdmin, validations.scheduleValidation.createScheduleValidation, middlewares.bodyMiddleware, controllers.api.v1.schedule.createSchedule);
+apiRouter.patch("/api/v1/admin/schedule/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.schedule.updateSchedule);
+apiRouter.delete("/api/v1/admin/schedule/:id", middlewares.authMiddleware.authAdmin, controllers.api.v1.schedule.deleteSchedule);
 
 /**
  * TODO: Delete this, this is just a demonstration of
